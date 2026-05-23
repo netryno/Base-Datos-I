3fea78c29a50b18a3840505b876ca278331588fa
Win Alvarez <windsoralvarezdavila@gmail.com>
2026-05-23 08:43:41 -0400

    mas avances

--- 06 recuperatorio/consultas.sql
+++ 06 recuperatorio/consultas.sql
@@ -1,3 +1,4 @@
+-- 1.- Gestion de BAse de datos
 -- Crear BD
 create database bd_prueba
 
@@ -6,3 +7,25 @@ drop database bd_prueba
 
 -- Para evitar errores
 drop database IF EXISTS bd_prueba
+
+-- 2.- Gestion de tablas
+CREATE TABLE personas (
+	id SERIAL PRIMARY KEY,
+	nombre VARCHAR(50) NOT NULL
+)
+
+DROP TABLE personas;
+
+-- insertar
+INSERT INTO  personas(nombre)
+VALUES
+('Juan Perez'),
+('Pedro Aguilar'),
+('Anabel Gutierrez');
+
+--listar
+select * from personas
+
+
+-- vaciar
+TRUNCATE TABLE personas
